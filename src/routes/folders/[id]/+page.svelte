dea87116853cdddf9f68f2807b2056ec66bd7b88
Adarsh <adarshmishra0701@gmail.com>
2024-06-19 19:46:54 +0530

    get folders by id

--- src/routes/folders/[id]/+page.svelte
+++ src/routes/folders/[id]/+page.svelte
@@ -6,37 +6,58 @@
 	import GridLayoutFolders from '../components/GridLayoutFolders.svelte';
 	import TableLayoutFolders from '../components/TableLayoutFolders.svelte';
 	import SidebarLayout from '$lib/components/layouts/SidebarLayout.svelte';
+	import { onMount } from 'svelte';
+	import { getDirectoryByIDInternal } from '$lib/api/services-internal';
+	import { page } from '$app/stores';
 
 	let tabValue: string = 'grid';
+	let folders: any = [];
 
-	const folders = new Array(5).fill(null).map((_, index) => ({
-		id: index.toString(),
-		name: `Dush Product ${index + 1}`,
-		parentId: '1',
-		createdAt: new Date().toISOString()
-	}));
+	const directoryId = $page.params.id;
+
+	const getDirectoryItems = async () => {
+		const res = await getDirectoryByIDInternal(directoryId);
+
+		if (res.status !== 200) {
+			return;
+		}
+
+		const tempFolders: any[] = [];
+		res.data.data.forEach((folder: any) => {
+			tempFolders.push({
+				id: folder.id,
+				...folder.attributes
+			});
+		});
+
+		folders = tempFolders;
+	};
+
+	onMount(() => {
+		getDirectoryItems();
+	});
 </script>
 
 <SidebarLayout pageTitle="All Files">
-<Tabs.Root bind:value={tabValue}>
-	<div class="flex w-full items-center justify-between">
-		<div>
-			<h5>Folders</h5>
+	<Tabs.Root bind:value={tabValue}>
+		<div class="flex w-full items-center justify-between">
+			<div>
+				<h5>Folders</h5>
+			</div>
+			<Tabs.List>
+				<Tabs.Trigger value="grid">
+					<GridIcon />
+				</Tabs.Trigger>
+				<Tabs.Trigger value="list">
+					<ListChecksIcon />
+				</Tabs.Trigger>
+			</Tabs.List>
 		</div>
-		<Tabs.List>
-			<Tabs.Trigger value="grid">
-				<GridIcon />
-			</Tabs.Trigger>
-			<Tabs.Trigger value="list">
-				<ListChecksIcon />
-			</Tabs.Trigger>
-		</Tabs.List>
-	</div>
-	<Tabs.Content value="grid" class="mt-4">
-		<GridLayoutFolders {folders} />
-	</Tabs.Content>
-	<Tabs.Content value="list">
-		<TableLayoutFolders {folders} />
-	</Tabs.Content>
-</Tabs.Root>
+		<Tabs.Content value="grid" class="mt-4">
+			<GridLayoutFolders {folders} />
+		</Tabs.Content>
+		<Tabs.Content value="list">
+			<TableLayoutFolders {folders} />
+		</Tabs.Content>
+	</Tabs.Root>
 </SidebarLayout>
